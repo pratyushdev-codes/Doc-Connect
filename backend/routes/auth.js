e0cc47b45e1ef346c86daf0ee3df1d3ae6a87603
Pratyush Birole <birole.pratyush@gmail.com>
2024-01-15 23:34:22 +0530

    Routes and Models, Validated Database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+
 
 // Creating a user using POST: "/api/auth/."
 
@@ -10,8 +12,10 @@ const router = express.Router();
 
 
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
 console.log(req.body);
+const user= User(req.body);
+user.save()
 res.send(req.body);
  
  });
